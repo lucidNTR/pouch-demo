def33cfa7ace6a412825298d5889dd053b0811f6
lucidNTR <git@stackblitz.com>
2025-05-20 18:01:06 +0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import test from 'pouchdb-browser'
+  console.log(test)
+  window.test = test
+  // console.log(pouchDB)
+  // const db = new PouchDB('my_db')
+</script>
+
+<main>
+  test
+</main>
+
+<style>
+
+</style>
