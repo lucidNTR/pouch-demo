4b2b07139944d3839e87192fad684008bd20edd5
Jan Johannes <jan@ntr.io>
2025-05-26 16:22:54 +0200

    add cors

--- functions/_couch.js
+++ functions/_couch.js
@@ -3,7 +3,11 @@ export const onRequest = async (context) => {
     const url = new URL(context.request.url)
 
     const headers = new Headers(context.request.headers)
-    headers.set('Authorization', `Basic ${btoa('localfirst:localfirst')}`);
+    headers.set('Authorization', `Basic ${btoa('localfirst:localfirst')}`)
+
+    headers.set('Access-Control-Allow-Origin', 'http://localhost:5173, https://stackblitz.com') 
+    headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
 
     return fetch(couchUrl + url.pathname.replace('/_couch', '/'), {
         method: context.request.method,
