b4ff639036ae6b4680eae20e56e79f2e00c32f0a
Jan Johannes <jan@ntr.io>
2025-06-06 00:45:02 +0300

    switch to env var

--- functions/_couch/[[catchall]].js
+++ functions/_couch/[[catchall]].js
@@ -24,7 +24,7 @@ export const onRequest = async (context) => {
     }
 
     const headers = new Headers(context.request.headers)
-    headers.set('Authorization', `Basic ${btoa('localfirst:localsecond')}`)
+    headers.set('Authorization', `Basic ${btoa('localfirst:' + context.env.COUCH_DEMO_PASSWORD)}`)
 
     const response = await fetch(couchUrl + url.pathname.replace('/_couch', '/') + (url.search ? `?${url.search}` : ''), {
         method: context.request.method,
